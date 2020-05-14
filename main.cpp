24e4dbb57513f404cf9549ac137f7c80db267cdd
gioretikto <gioresta87@gmail.com>
2020-05-14 12:29:48 +0200

    first commmit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "game.h"
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	Game game;
+	
+	bool success = game.Initialize();
+	
+	if (success)
+	{
+		game.RunLoop();
+	}
+	
+	game.Shutdown();
+	
+	return 0;
+}
